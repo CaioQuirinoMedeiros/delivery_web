81459930dadae6eae4907835d52a24b14da3da58
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-01 16:47:09 -0300

    Global styles

---
+++ src/styles/index.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #353940;
+    color: #fff;
+    font-family: 'Helvetica', "Arial", sans-serif;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  html, body, #root {
+    height: 100%
+  }
+
+  input, button {
+    font-family: 'Helvetica', "Arial", sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+  }
+`;
