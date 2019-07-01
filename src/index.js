9a92c36a56120cee81ef3e6dd203ce19025205b1
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-01 16:47:27 -0300

    Starting dotenv in the root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dotenv from 'dotenv';
+
 import App from './App';
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
