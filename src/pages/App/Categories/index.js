3071f92b24134d8a39e99fea6678c8d19686db61
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-02 00:37:28 -0300

    Startging categories page

--- src/pages/App/Categories/index.js
+++ src/pages/App/Categories/index.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+import api from "../../../services/api";
 // import { Container } from './styles';
 
 class Categories extends Component {
-  componentDidMount() {}
+  state = {
+    categories: []
+  };
+
+  componentDidMount() {
+    this.loadCategories();
+  }
+
+  loadCategories = async () => {
+    try {
+      const { data } = await api.get("admin/categories");
+
+      this.setState({ categories: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   render() {
-    return <div>CATEGORIAS</div>;
+    const { categories } = this.state;
+    return (
+      <div>
+        {categories.map(category => (
+          <h2 key={category.id}>{category.name}a</h2>
+        ))}
+      </div>
+    );
   }
 }
 
