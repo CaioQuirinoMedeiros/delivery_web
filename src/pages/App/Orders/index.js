9158b3318b8b588f9673da39d333a89753b0c301
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-02 00:37:39 -0300

    Orders page finished

--- src/pages/App/Orders/index.js
+++ src/pages/App/Orders/index.js
@@ -1,12 +1,134 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { distanceInWordsToNow } from "date-fns";
+import pt from "date-fns/locale/pt";
 
-// import { Container } from './styles';
+import { convertToBRL } from "../../../services/currency";
+import api from "../../../services/api";
+
+import {
+  Container,
+  OrderCard,
+  ItemsContainer,
+  ItemCard,
+  Filters
+} from "./styles";
 
 class Orders extends Component {
-  componentDidMount() {}
+  state = {
+    orders: [],
+    filters: {
+      pendente: true,
+      cancelado: false,
+      pago: true,
+      enviado: true,
+      finalizado: false
+    }
+  };
+
+  componentDidMount() {
+    this.loadOrders();
+  }
+
+  loadOrders = async () => {
+    try {
+      const { data } = await api.get("admin/orders");
+
+      this.setState({ orders: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  updateOrderStatus = async (id, status) => {
+    try {
+      await api.put(`admin/orders/${id}`, { status });
+
+      await this.loadOrders();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  toggleFilter = filter => {
+    this.setState({
+      filters: { ...this.state.filters, [filter[0]]: !filter[1] }
+    });
+  };
+
+  renderFilters = () => {
+    const { filters } = this.state;
+
+    return (
+      <Filters>
+        {Object.entries(filters).map(filter => (
+          <div
+            onClick={() => this.toggleFilter(filter)}
+            key={filter[0]}
+            className={filter[1] ? "active" : ""}
+          >
+            {filter[0]}
+          </div>
+        ))}
+      </Filters>
+    );
+  };
+
+  renderItem = item => (
+    <ItemCard key={item.id}>
+      <img src={item.product_size.product.image.url} alt="product" />
+      <div>
+        <span>{item.product_size.product.name}</span>
+        <p>Tamanho: {item.product_size.size.name}</p>
+        <p>Quantidade: {item.quantity}</p>
+      </div>
+    </ItemCard>
+  );
 
   render() {
-    return <div>ORDERS</div>;
+    const { orders, filters } = this.state;
+    return (
+      <Container>
+        {this.renderFilters()}
+        {orders.map(order =>
+          filters[order.status] ? (
+            <OrderCard key={order.id} status={order.status}>
+              <div className="orderHeader">
+                <h2>
+                  Pedido <strong>#{order.id}</strong> - {order.user.name}
+                </h2>
+                <select
+                  name="status"
+                  value={order.status}
+                  onChange={e =>
+                    this.updateOrderStatus(order.id, e.target.value)
+                  }
+                >
+                  <option value="pendente">pendente</option>
+                  <option value="cancelado">cancelado</option>
+                  <option value="enviado">enviado</option>
+                  <option value="pago">pago</option>
+                  <option value="finalizado">finalizado</option>
+                </select>
+              </div>
+              <p>
+                {distanceInWordsToNow(order.created_at, {
+                  locale: pt,
+                  addSuffix: true
+                })}
+              </p>
+              <strong>{convertToBRL(Number(order.total))}</strong>
+              <ItemsContainer>
+                {order.items.map(item => this.renderItem(item))}
+              </ItemsContainer>
+              <span>
+                <strong>Observações: </strong>
+                {order.observations}
+              </span>
+            </OrderCard>
+          ) : null
+        )}
+      </Container>
+    );
   }
 }
 
---
+++ src/pages/App/Orders/styles.js
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+export const OrderCard = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  margin: 10px 0;
+
+  box-shadow: 0px 0px 12px -5px rgba(0, 0, 0, 0.75);
+  border-radius: 10px;
+
+  & > div.orderHeader {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h2 {
+      font-size: 18px;
+      font-weight: normal;
+    }
+
+    select {
+      cursor: pointer;
+      padding: 5px;
+      border: none;
+      border-radius: 10px;
+      font-weight: bold;
+      color: #fff;
+      opacity: 0.7;
+      transition: all 0.3s;
+      background: ${({ status }) =>
+        status === "pendente"
+          ? "orange"
+          : status === "cancelado"
+          ? "red"
+          : "green"};
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+
+  & > p {
+    font-size: 12px;
+    color: #706e7b;
+    margin: 8px 0;
+  }
+
+  strong {
+    font-size: 16px;
+  }
+
+  & > span {
+    font-size: 14px;
+    color: #706e7b;
+  }
+`;
+
+export const ItemsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 20px 0 10px;
+  margin: 10px 0;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+`;
+
+export const ItemCard = styled.div`
+  display: flex;
+  width: 230px;
+  flex-shrink: 0;
+  padding: 10px;
+  margin: 0 10px 10px 0;
+
+  border-radius: 10px;
+  border: 1px solid #ddd;
+
+  & > img {
+    width: 60px;
+    height: 60px;
+  }
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 8px;
+
+    span {
+      font-size: 13px;
+    }
+
+    p {
+      margin-top: 5px;
+      font-size: 11px;
+      color: #706e7b;
+    }
+  }
+`;
+
+export const Filters = styled.div`
+  align-self: flex-start;
+  display: flex;
+  margin: 15px 0;
+
+  & > div {
+    width: 90px;
+    text-align: center;
+    padding: 10px 5px;
+    border-radius: 10px;
+    border: 1px solid #ddd;
+    margin-right: 10px;
+    cursor: pointer;
+    transition: all 0.3s;
+
+    &:hover,
+    &.active {
+      background: #f15454;
+      border-color: #f15454;
+      color: #fff;
+    }
+  }
+`;
