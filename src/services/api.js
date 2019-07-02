2d05a0e9d74a2515c33eaa34bf0417bc24eb9205
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-02 00:38:07 -0300

    Adding interceptor for authentication

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-param-reassign */
 import axios from 'axios';
+
+import { getToken } from './auth';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
