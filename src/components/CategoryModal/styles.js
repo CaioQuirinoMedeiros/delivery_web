7a9896a5b227f4b7c68617071210eac84bec5563
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-03 07:16:31 -0300

    Background darker and margin to title

--- src/components/CategoryModal/styles.js
+++ src/components/CategoryModal/styles.js
@@ -9,7 +9,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.7);
   z-index: 99;
 `;
 
@@ -23,6 +23,7 @@ export const CategoryForm = styled.form`
 
   h2 {
     text-align: center;
+    margin-bottom: 10px;
   }
 
   span {
