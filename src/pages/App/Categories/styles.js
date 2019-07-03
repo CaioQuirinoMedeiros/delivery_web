33c3014df61f1df2f12d19f5675bbef869102650
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-03 09:28:03 -0300

    Changing overflow to scroll: no more flicking scrollbar

--- src/pages/App/Categories/styles.js
+++ src/pages/App/Categories/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  overflow-y: auto;
+  overflow-y: scroll;
   flex-direction: column;
   align-items: center;
   padding: 25px 50px;
--- src/pages/App/Images/styles.js
+++ src/pages/App/Images/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  overflow-y: auto;
+  overflow-y: scroll;
   flex-direction: column;
   padding: 25px 50px;
 `;
