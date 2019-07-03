5549abaa7b559fc858578e29f526912c2726af8c
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-03 03:18:26 -0300

    Size modal

---
+++ src/components/SizeModal/index.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+
+import api from "../../services/api";
+import { Container, SizeForm } from "./styles";
+
+class SizeModal extends Component {
+  state = {
+    image_id: "",
+    name: "",
+    multiplier: "",
+    category_id: "",
+    isLoading: false,
+    error: "",
+    images: [],
+    categories: []
+  };
+
+  componentDidMount() {
+    this.loadImages();
+    this.loadCategories();
+  }
+
+  loadImages = async () => {
+    try {
+      const { data } = await api.get("admin/images");
+
+      this.setState({ images: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  loadCategories = async () => {
+    try {
+      const { data } = await api.get("admin/categories");
+
+      this.setState({ categories: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value, error: "" });
+  };
+
+  handleCreateSize = async e => {
+    e.preventDefault();
+
+    const { name, multiplier, image_id, category_id } = this.state;
+    const { closeModal } = this.props;
+
+    try {
+      this.setState({ isLoading: true });
+
+      await api.post("admin/sizes", {
+        name,
+        multiplier,
+        image_id,
+        category_id
+      });
+
+      closeModal();
+    } catch (err) {
+      console.log(err);
+      this.setState({ error: "Não foi possível criar o tamanho" });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    const {
+      name,
+      multiplier,
+      isLoading,
+      error,
+      images,
+      categories
+    } = this.state;
+    const { closeModal } = this.props;
+
+    console.log(this.state);
+
+    return (
+      <Container>
+        <SizeForm onSubmit={this.handleCreateSize}>
+          <h2>Criar novo tamanho</h2>
+          {!!error && <span>{error}</span>}
+          <input
+            name="name"
+            value={name}
+            onChange={this.handleInputChange}
+            placeholder="Nome"
+          />
+          <input
+            name="multiplier"
+            type="number"
+            min="0"
+            max="10"
+            step="0.01"
+            value={multiplier}
+            onChange={this.handleInputChange}
+            placeholder="Multipicador de preço"
+          />
+          <div>
+            <label>Imagem</label>
+            <select
+              name="image_id"
+              onChange={e => this.setState({ image_id: e.target.value })}
+            >
+              {images.length &&
+                images.map(image => (
+                  <option key={image.id} value={image.id}>
+                    {image.original_name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div>
+            <label>Categoria</label>
+            <select name="category_id" onChange={this.handleInputChange}>
+              {categories.length &&
+                categories.map(category => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <button type="submit">
+            {isLoading ? "Carregando..." : "Enviar"}
+          </button>
+          <button type="button" className="close" onClick={() => closeModal()}>
+            Fechar
+          </button>
+        </SizeForm>
+      </Container>
+    );
+  }
+}
+
+export default SizeModal;
---
+++ src/components/SizeModal/styles.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 99;
+`;
+
+export const SizeForm = styled.form`
+  display: flex;
+  width: 400px;
+  flex-direction: column;
+  padding: 30px;
+  background: #fff;
+  border-radius: 10px;
+
+  h2 {
+    text-align: center;
+  }
+
+  span {
+    margin: 5px 0;
+    font-size: 13px;
+    color: #e62638;
+    text-align: center;
+  }
+
+  & > div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+
+    label {
+      color: #757575;
+      margin-right: 15px;
+    }
+
+    select {
+      border-radius: 10px;
+      border: 1px solid #ddd;
+
+      padding: 15px;
+      flex: 1;
+    }
+  }
+
+  input {
+    width: 100%;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    padding: 15px;
+    margin-top: 10px;
+  }
+
+  button {
+    width: 100%;
+    margin-top: 20px;
+    text-align: center;
+    padding: 15px;
+    border-radius: 10px;
+    background: #f15454;
+    color: #fff;
+    font-weight: bold;
+    transition: all 0.2s;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+
+  button.close {
+    margin-top: 10px;
+    background: #fff;
+    color: inherit;
+    padding: 5px;
+  }
+`;
